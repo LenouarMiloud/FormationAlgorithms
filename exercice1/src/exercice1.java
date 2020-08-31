c41c7c4e231ac9802eb3117ac9a09ff79e478214
LenouarMiloud <lenouarmiloud@gmail.com>
2020-08-31 16:10:05 +0100

    add the first exercice

---
+++ exercice1/src/exercice1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class exercice1 {
+
+    public static void main(String[] args) {
+        int celsuis;
+        double fahrenheit;
+        Scanner clavier = new Scanner(System.in);
+
+        System.out.print("Enter the value of Celsuis :");
+        celsuis = clavier.nextInt();
+
+        fahrenheit = (celsuis * 1.8 ) + 32;
+
+        System.out.println("Fahrenheit = "+fahrenheit);
+
+    }
+}

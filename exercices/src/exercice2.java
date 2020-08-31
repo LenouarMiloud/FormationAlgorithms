1df7bc8969176a2bdc5571317c7a0ece6f746cef
LenouarMiloud <lenouarmiloud@gmail.com>
2020-08-31 21:59:20 +0100

    delete some files

---
+++ exercices/src/exercice2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class exercice2 {
+    public static void main(String[] args) {
+        int nb1,nb2,nb3;
+        Scanner clavier = new Scanner(System.in);
+        System.out.print("Enter the value of the 3 numbers :");
+        nb1 = clavier.nextInt();
+        nb2 = clavier.nextInt();
+        nb3 = clavier.nextInt();
+        //test the numbers
+        if ((nb1>nb2) && (nb1>nb3)) {
+            System.out.println(nb1+" is the bigest one");
+        }else if ((nb2>nb1) && (nb2>nb3)){
+            System.out.println(nb2+" is the bigest one");
+        }else
+            System.out.println(nb3+" is the bigest one");
+        //end of the program
+    }
+}
